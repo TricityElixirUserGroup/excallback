02160a8d08aacc7ca55a0c381c520d397f24429c
Bartosz Markowski <nenros@gmail.com>
2018-01-21 17:35:15 +0100

    Make it like in ace example

--- lib/excallback/application.ex
+++ lib/excallback/application.ex
@@ -6,14 +6,9 @@ defmodule Excallback.Application do
   import Supervisor.Spec
 
   def start(_type, _args) do
-    options = [
-      port: Application.get_env(:excallback, :port),
-      connections: 1_000,
-      cleartext: true
-    ]
     children = [
-      {Ace.HTTP.Service, [{Excallback.Proxy, []}, options]},
-      worker(Excallback.Database, [])
+      {Excallback.Proxy, [%{}, [port: Application.get_env(:excallback, :port)]]},
+      worker(Excallback.Database, []),
     ]
 
     opts = [strategy: :one_for_one, name: Excallback.Supervisor]
