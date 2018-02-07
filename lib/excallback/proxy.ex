c8e31f9bb6e776fa93712a6d375c69630493569b
Bartosz Markowski <nenros@gmail.com>
2018-02-07 11:13:29 +0100

    Make it like in ace example (#1)

--- lib/excallback/proxy.ex
+++ lib/excallback/proxy.ex
@@ -1,33 +1,34 @@
 defmodule Excallback.Proxy do
-  use Raxx.Server
-#  use Raxx.Static, "./public"
+  use Ace.HTTP.Service, cleartext: true
+  #  use Raxx.Static, "./public"
 
-  @fake Mix.env == :fake
+  @fake Mix.env() == :fake
 
-  @impl Raxx.Server
   def handle_request(request, _) do
     if @fake do
-      Raxx.response(:ok)
-      |> Raxx.set_body("Fake responese")
+      response(:ok)
+      |> set_body("Fake responese")
     else
-      IO.inspect(request)
-
       method = request.method |> Atom.to_string() |> String.downcase() |> String.to_atom()
       orig_uri = URI.parse("#{request.scheme}://#{request.authority}")
-      target_uri = orig_uri |> Map.put(:port, 80) |> URI.to_string
+      target_uri = orig_uri |> Map.put(:port, 80) |> URI.to_string()
       response = apply(HTTPoison, method, [target_uri])
 
       IO.inspect(response)
 
       case response do
         {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-          IO.puts body
+          IO.puts(body)
+
         not_200_response ->
           Excallback.Database.inout_add(%{request: request, response: not_200_response})
       end
 
-      Raxx.response(:ok)
-      |> Raxx.set_body(response.body)
+      response(:ok)
+      |> set_body(get_body(response))
     end
   end
+
+  defp get_body({:ok, %HTTPoison.Response{body: body}}), do: body
+  defp get_body(_), do: "Error message"
 end
